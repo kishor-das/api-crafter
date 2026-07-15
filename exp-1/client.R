2fb6507a299e4675061a7b48c5e41603b6c081ab
Kishor Das <5061926+kishor-das@users.noreply.github.com>
2026-07-15 14:01:42 +0100

    adding POST /output endpoint

--- exp-1/client.R
+++ exp-1/client.R
@@ -25,3 +25,16 @@ post_resp <- req_perform(post_req)
 # 3. Unpack the response body
 resp_body_json(post_resp)
 # Returns: list(status = "created", received_data = list(name = "Running Shoes", price = 89.99))
+
+
+
+
+post_req <- request("http://127.0.0.1:8000/output") |> 
+  req_body_json(list(code = "version"))
+
+# 2. Perform the request
+post_resp <- req_perform(post_req)
+
+# 3. Unpack the response body
+resp_body_json(post_resp)
+# Returns: list(status = "created", received_data = list(name = "Running Shoes", price = 89.99))
--- exp-1/server.R
+++ exp-1/server.R
@@ -1,6 +1,7 @@
 library(plumber)
 
 #* Search for an item
+#* @serializer json
 #* @get /items
 function(category = "all") {
   # Plumber parses 'category' from the URL query string
@@ -11,16 +12,35 @@ function(category = "all") {
 }
 
 #* Add a new item
+#* @serializer json
 #* @post /items
 function(req) {
   # Plumber parses 'req$body' from the incoming JSON payload
   new_item <- req$body
-  
+
   list(
     status = "created",
     received_data = new_item
   )
 }
 
+
+#* Add a new item
+#* @serializer json
+#* @post /output
+function(code, req) {
+  if (is.null(code)) {
+    out <- "No code provided"
+  } else {
+    out <- capture.output(eval(parse(text = code)))
+  }
+  client_ip <- req$REMOTE_ADDR
+
+  list(
+    status = "created",
+    output = out,
+    ip = client_ip 
+  )
+}
 
 # pr("exp-1/server.R") |> pr_run(port = 8000)
